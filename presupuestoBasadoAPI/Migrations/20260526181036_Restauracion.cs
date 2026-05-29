f5d11f4f1f5752d0df3d096b9ddc36534215aa85
RAMONSYSTEM\jisaw <jisaw_luis@hotmail.es>
2026-05-29 10:07:22 -0600

    migraciones no funcionales, simplemente intentaba restaurar algunos sectores de la base de datos, de todas formas funciona de manera correcta

---
+++ presupuestoBasadoAPI/Migrations/20260526181036_Restauracion.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace presupuestoBasadoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Restauracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FilaMatriz_MatricesIndicadores_MatrizIndicadoresId",
+                table: "FilaMatriz");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MatrizIndicadoresId",
+                table: "FilaMatriz",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FilaMatriz_MatricesIndicadores_MatrizIndicadoresId",
+                table: "FilaMatriz",
+                column: "MatrizIndicadoresId",
+                principalTable: "MatricesIndicadores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FilaMatriz_MatricesIndicadores_MatrizIndicadoresId",
+                table: "FilaMatriz");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MatrizIndicadoresId",
+                table: "FilaMatriz",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FilaMatriz_MatricesIndicadores_MatrizIndicadoresId",
+                table: "FilaMatriz",
+                column: "MatrizIndicadoresId",
+                principalTable: "MatricesIndicadores",
+                principalColumn: "Id");
+        }
+    }
+}
